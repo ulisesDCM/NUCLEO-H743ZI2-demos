3e94af79fdc379c1e2b5abd85443db9895931ec4
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-14 17:41:49 -0800

    Added some notes in the reference manual and removing old comments in the main.c file

--- CMSIS-Blink/Src/main.c
+++ CMSIS-Blink/Src/main.c
@@ -26,6 +26,8 @@ static void mcu_error(void);
 int main(void)
 {
 	int mcu_status = 0;
+	uint32_t x,y,res;
+
 
 //	Initialize the power supply
 	mcu_status = mcu_pwr_init();
@@ -53,54 +55,12 @@ int main(void)
 	for(;;);
 }
 
-/*
- * 1. When the system is powered on, the POR monitors VDD supply. Once VDD is above the
- * POR threshold level, the voltage regulator is enabled in the default supply
- * configuration:
- * 		The Voltage converter output level is set at 1.0 V in accordance with the VOS3
- * 		level configured in PWRD3 domain control register (PWR_D3CR).
- *
- * 	2. The system is kept in reset mode as long as VCORE is not correct.
- *
- * 	3. Once VCORE is correct, the system is taken out of reset and the HSI oscillator is
- * 	enabled.
- *
- * 	4. Once the oscillator is stable, the system is initialized: Flash memory and option bytes
- * 	are loaded and the CPU starts in limited run mode (Run*).
- *
- * 	5. The software shall then initialize the system including supply configuration
- * 	programming in PWR control register 3 (PWR_CR3). Once the supply configuration
- * 	has been configured, the ACTVOSRDY bit in PWR control status register 1
- * 	(PWR_CSR1) shall be checked to guarantee valid voltage levels:
- *
- * 		a) As long as ACTVOSRDY indicates that voltage levels are invalid, tError Code Correctionhe system is in
- * 		Run* mode, write accesses to the RAMs are not permitted and VOS shall not be
- * 		changed.
- *
- * 		b)Once ACTVOSRDY indicates that voltage levels are valid, the system is in normal
- * 		Run mode, write accesses to RAMs are allowed and VOS can be changed.
- * */
 static int mcu_pwr_init(void)
 {
 	int ret = 0;
-	/*
-		For the moment I will keep the default configuration using Domain3 and Scale3 and the
-		LDO enabled.
-	 */
 	return ret;
 }
 
-/*
- * 	This section will adjust the read timing constraints
- * 	The embedded flash memory clock must be enabled and running before reading data from
- * 	non-volatile memory.
- *
- * 	To correctly read data from flash memory, the number of wait states (LATENCY) must be
- * 	correctly programmed in the flash access control register (FLASH_ACR) according to the
- * 	embedded flash memory AXI interface clock frequency (sys_ck) and the internal voltage
- * 	range of the device (V core ).
- * 	For more information, please refer to table 17. on page 160, of the reference manual.
- * */
 static int mcu_flash_init(void)
 {
 	int ret = 0;
