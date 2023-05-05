718daaff77f92926f6fbb0dc5645abdb91fa6b03
ulises <ulises.cmorales@hotmail.com>
2023-05-05 12:27:54 -0700

    Cleaning the base code to configure GPIOB and added new function to toogle each LED.

--- RTX-helloWorld/main.c
+++ RTX-helloWorld/main.c
@@ -10,36 +10,54 @@
 		
 */
 #include "stm32h7xx.h"
+typedef enum
+{
+	GREEN = GPIO_ODR_OD0_Pos,
+	BLUE = GPIO_ODR_OD7_Pos,
+	RED = GPIO_ODR_OD14_Pos
+}eUSER_LED;
 
 static void init_led_gpio(void)
 {
-	int i=0;
-	
-	RCC->AHB4RSTR |= RCC_AHB4RSTR_GPIOBRST;		  //Reset GPIOB
-	for(i=0; i<10000;i++);	//Some delay until the GPIOB resets.
-	RCC->AHB4RSTR &= ~RCC_AHB4RSTR_GPIOBRST;		//Reset GPIOB
-		
-	RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN;	//Enabling GPIOB peripheral clock
-	
-	// Configure GPIOB
+	SET_BIT(RCC->AHB4ENR, RCC_AHB4ENR_GPIOBEN);	//Enable GPIOB peripheral clock
 	
 	// OUTPUT MODE
-	GPIOB->MODER = 0;
-	GPIOB->MODER |= (GPIO_MODER_MODE0_0 | GPIO_MODER_MODE7_0 | GPIO_MODER_MODE14_0);
+	MODIFY_REG(GPIOB->MODER, 
+						(GPIO_MODER_MODE0 | GPIO_MODER_MODE7 | GPIO_MODER_MODE14), 
+						(GPIO_MODER_MODE0_0 | GPIO_MODER_MODE7_0 | GPIO_MODER_MODE14_0)
+	);
 	
 	//PUSH-PULL
-	GPIOB->OTYPER = 0;
+	CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT0);
+	CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7);
+	CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT14);
 	
-	//Set the GPIO State
-	GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD7 | GPIO_ODR_OD14);
+	//Turn off all LEDs
+	CLEAR_BIT(GPIOB->ODR, GPIO_ODR_OD0 );
+	CLEAR_BIT(GPIOB->ODR, GPIO_ODR_OD7 );
+	CLEAR_BIT(GPIOB->ODR, GPIO_ODR_OD14 );	
 }
+
+static void toogle_led(eUSER_LED led)
+{
+	int state = READ_BIT(GPIOB->ODR, (1<<led)) >> led;
+	state = !state & 1;
+	MODIFY_REG(GPIOB->ODR, (1<<led), (state<<led));
+}	
 
 int main(void)
 {
 	init_led_gpio();
 	
 	while(1)
-	{		
-	}
+	{
+		toogle_led(GREEN);
+		toogle_led(GREEN);
 
+		toogle_led(RED);
+		toogle_led(RED);
+		
+		toogle_led(BLUE);
+		toogle_led(BLUE);
+	}
 }
